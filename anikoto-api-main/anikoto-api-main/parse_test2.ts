abe0d4816dfea2063df97f585d5b15fddfec7955
shadowanimexyz <shadowanimexyz@gmail.com>
2026-05-22 17:47:56 +0530

    Delete anikoto-api-main/anikoto-api-main directory

--- anikoto-api-main/anikoto-api-main/parse_test2.ts
+++
@@ -1,7 +0,0 @@
-import * as cheerio from "cheerio";
-import fs from "fs";
-
-const html = fs.readFileSync("search.html", "utf-8");
-const $ = cheerio.load(html);
-
-console.log($('.item').first().html());
